8ae8df3cf61c86dc28f07a2919079d34bc95427f
wesleywil <wesleywilsonti@gmail.com>
2023-08-05 13:19:40 -0300

    added some visual-only pages: dashboard and profile_edit

---
+++ app/Views/pages/dashboard.php
@@ -0,0 +1,46 @@
+<div class="w-full h-full flex flex-col items-center text-center">
+    <h1 class="mt-8 text-4xl font-bold">Dashboard</h1>
+    <div class="w-full mt-8">
+        <img src="https://dummyimage.com/100x100" alt="profile" class="mx-auto rounded-full">
+        <h1>Wesley Wilson</h1>
+        <h2>dummyemail@mail.com</h2>
+        <h1 class="mt-4">Palette of colors</h1>
+        <div class="w-1/2 mx-auto mt-2 h-8 flex rounded overflow-hidden">
+            <span class="grow h-8 bg-black"></span>
+            <span class="grow h-8 bg-white"></span>
+            <span class="grow h-8 bg-purple-500"></span>
+        </div>
+        <div class="flex flex-wrap justify-center">
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+            <div class="p-1 flex items-center text-xs">
+                <img src="https://dummyimage.com/40x40" alt="social media" class="rounded-full">
+                <span class="ml-1">@dummysocialmedia</span>
+            </div>
+        </div>
+        <div class="mt-4">
+            <a href="/profile_edit"
+                class=" px-2 py-1 text-2xl text-white font-bold bg-purple-500 hover:bg-purple-700 rounded">Edit
+                Profile</a>
+        </div>
+
+    </div>
+</div>
---
+++ app/Views/pages/profile_edit.php
@@ -0,0 +1,46 @@
+<div class="w-full pb-4 flex flex-col items-center text-center">
+    <h1 class="mt-8 text-4xl font-bold">Profile Edit</h1>
+
+    <h2 class="mt-1 text-xl font-semibold">Personal Info</h2>
+    <form class="w-11/12 p-2 flex flex-col gap-1 bg-slate-200 rounded">
+        <input type="text" placeholder="Name" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Username" class="w-full px-2 py-1 rounded">
+        <input type="email" placeholder="E-mail" class="w-full px-2 py-1 rounded">
+        <div class="mt-1 flex gap-4 justify-center">
+            <button class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Submit</button>
+            <button type="button"
+                class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Cancel</button>
+        </div>
+    </form>
+    <h2 class="mt-1 text-xl font-semibold">Color Palette</h2>
+    <form class="w-11/12 p-2 flex flex-col gap-1 bg-slate-200 rounded">
+        <div class="flex gap-1 px-2 py-1 bg-white rounded">
+            <span class="w-2/3 text-left text-gray-400">Background Color</span> <input type="color">
+        </div>
+        <div class="flex gap-1 px-2 py-1 bg-white rounded">
+            <span class="w-2/3 text-left text-gray-400">Text Color</span> <input type="color">
+        </div>
+        <div class="flex gap-1 px-2 py-1 bg-white rounded">
+            <span class="w-2/3 text-left text-gray-400">Accent Color</span> <input type="color">
+        </div>
+        <div class="mt-1 flex gap-4 justify-center">
+            <button class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Submit</button>
+            <button type="button"
+                class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Cancel</button>
+        </div>
+    </form>
+    <h2 class="mt-1 text-xl font-semibold">Social Medias (@)</h2>
+    <form class="w-11/12 p-2 flex flex-col gap-1 bg-slate-200 rounded">
+        <input type="text" placeholder="Twitter(X)" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Facebook" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Instagram" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Linkedin" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Youtube" class="w-full px-2 py-1 rounded">
+        <input type="text" placeholder="Tiktok" class="w-full px-2 py-1 rounded">
+        <div class="mt-1 flex gap-4 justify-center">
+            <button class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Submit</button>
+            <button type="button"
+                class="px-2 py-1 text-white font-semibold bg-red-500 hover:bg-red-700 rounded">Cancel</button>
+        </div>
+    </form>
+</div>
