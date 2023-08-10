d0f8a90ac491865e9fb963392e4f02a92531f1cf
wesleywil <wesleywilsonti@gmail.com>
2023-08-10 12:45:57 -0300

    update Models by including the allowedFields that can be updated using the db transactions

--- app/Models/ColorsModel.php
+++ app/Models/ColorsModel.php
@@ -8,6 +8,8 @@ class ColorsModel extends Model
 {
     protected $table = 'social_aggregator.colors';
 
+    protected $allowedFields = ['txt_color', 'acc_color', 'bg_color', 'userId'];
+
     public function getColors($userId)
     {
         return $this->where(['userId' => $userId])->first();
--- app/Models/SocialsModel.php
+++ app/Models/SocialsModel.php
@@ -8,6 +8,8 @@ class SocialsModel extends Model
 {
     protected $table = 'social_aggregator.socials';
 
+    protected $allowedFields = ['facebook', 'twitter', 'tiktok', 'instagram', 'youtube', 'linkedin', 'userId'];
+
     public function getSocials($userId)
     {
         return $this->where(['userId' => $userId])->first();
